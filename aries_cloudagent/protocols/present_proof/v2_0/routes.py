5af6e50d104d415d5f850c6e1c2b6408fb41d33c
Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
2024-07-12 20:35:14 +0530

    VC DI proof request (#2960)
    * WIP: vc di proof request - authored by ianco(https://github.com/hyperledger/aries-cloudagent-python/pull/3043)
    
    Signed-off-by: Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
    
    * fixed lint checks, cleanup
    
    Signed-off-by: Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
    
    * fix: verify_pres, get_sign_key_credential_subject_id
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * WIP: debugging revocation & fixes
    
    Signed-off-by: Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
    
    * WIP: fix ununsed import
    
    Signed-off-by: Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
    
    * refactor: create_signed_anoncreds_presentation,   faber vcdi proof_request_web_request  object for revocation
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * Refactor:Add W3cCredential loading for VCDI format handler
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * fix: tests
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * WPA: using static data to test the revocation validation
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * feat: Add revocation support to VCDI
    - getting the correct timestamp
    - create  rev_states
    - remove static code
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * Remove unused code for credential definition and revocation
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * WPA: fix lint
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * Fix cred search for vc_di proof
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * Additional integration tests for vc_di and revocation
    
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>
    
    * refactor: remove unused comments and TODO's
     - implement _extract_cred_idx
    - add try catch to some expected fail code
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * refactor: split create_signed_anoncreds_presentation
    to
     - create_rev_states
    - prepare_data_for_presentation
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * refactor: `create_signed_anoncreds_presentation`
    
    add
    - prepare_data_for_presentation
    - _load_w3c_credentials functions
    
    remove holder flag
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * add: tests
    
    - test_assert_no_callenge_error
    - test_assert_verify_presentation
    - test__extract_cred_idx
    - test__get_predicate_type_and_value
    - test__load_w3c_credentials
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * add: tests, remove todos
    
    - test_create_presentation_w3c
    - test_create_presentation_w3c_create_error
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * add: tests
    
    - test_create_signed_anoncreds_presentation
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * fix: linter
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * fix: remove unused imports
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * add: tests
    
    - test_store_credential_w3c
    - test_get_type_manager_options
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    * refactor: remove extra variables and comments
    
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    
    ---------
    
    Signed-off-by: Sarthak Vijayvergiya <sarthakvijayvergiya@gmail.com>
    Signed-off-by: EmadAnwer <emadanwer.official@gmail.com>
    Signed-off-by: Emad <emadanwer.official@gmail.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>
    Co-authored-by: EmadAnwer <emadanwer.official@gmail.com>
    Co-authored-by: Ian Costanzo <ian@anon-solutions.ca>

--- aries_cloudagent/protocols/present_proof/v2_0/routes.py
+++ aries_cloudagent/protocols/present_proof/v2_0/routes.py
@@ -577,6 +577,7 @@ async def present_proof_credentials_list(request: web.BaseRequest):
                     extra_query,
                 )
             )
+
     except (IndyHolderError, AnonCredsHolderError) as err:
         if pres_ex_record:
             async with profile.session() as session:
@@ -708,6 +709,15 @@ async def present_proof_credentials_list(request: web.BaseRequest):
                                     ):
                                         proof_type = [BbsBlsSignature2020.signature_type]
                                         break
+                    elif claim_fmt.di_vc:
+                        if "proof_type" in claim_fmt.di_vc:
+                            proof_types = claim_fmt.di_vc.get("proof_type")
+
+                            proof_type = [
+                                "DataIntegrityProof"
+                            ]  # [LinkedDataProof.signature_type]
+
+                        # TODO check acceptable proof type(s) ("anoncreds-2023")
                     else:
                         raise web.HTTPBadRequest(
                             reason=(
